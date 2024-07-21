de193509155b9c096f9d544e5890f01eb9856a9e
isun-dev <florachoi1124@naver.com>
2024-07-21 10:05:38 +0900

    Initial commit

---
+++ src/test/java/com/hackseoul/aiwedding/AiWeddingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackseoul.aiwedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWeddingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

352cd8c7ed09d749c60cea792b2bb9ef28da9635
Jeongsik An <ajs15010120@gmail.com>
2024-07-23 23:07:01 +0900

    docs : mariadb run sh

---
+++ docker/mariadb-run.sh
@@ -0,0 +1,15 @@
+echo "docker image download and run database"
+
+docker run --name wedding-db \
+  -e MYSQL_ROOT_PASSWORD=1234 \
+  -e MYSQL_DATABASE=wedding \
+  -e MYSQL_USER=wedding \
+  -e MYSQL_PASSWORD=1234 \
+  -p 3306:3306 \
+  -d mariadb:latest
+
+#echo  "docker container start wedding-db "
+#docker start wedding-db
+
+#echo "docker container stop wedding-db "
+#docker stop wedding-db

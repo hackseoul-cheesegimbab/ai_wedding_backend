c5f24cc1a64456d54513fd07570521e7f023be79
isun-dev <florachoi1124@naver.com>
2024-08-24 16:35:13 +0900

    feat: 스웨거 수정

--- src/main/java/com/hackseoul/aiwedding/wedding/aichat/controller/AIChatController.java
+++ src/main/java/com/hackseoul/aiwedding/wedding/aichat/controller/AIChatController.java
@@ -3,7 +3,6 @@ package com.hackseoul.aiwedding.wedding.aichat.controller;
 import com.hackseoul.aiwedding.wedding.aichat.model.response.AIChatResponse;
 import com.hackseoul.aiwedding.wedding.aichat.service.AIChatService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,13 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "알림 API", description = "컨트롤러에 대한 설명입니다.")
+@Tag(name = "AI에 질의", description = "AI에 질의하는 API 입니다.")
 public class AIChatController {
     private final AIChatService aiChatService;
 
     @GetMapping("api/v1/ai/generate")
     @Operation(summary = "AI 메시지 생성", description = "주어진 프롬프트 메시지를 기반으로 AI가 응답을 생성합니다.")
-    @Parameter(name = "AI에게 전달할 메시지", description = "메시지를 입력하세요.")
     public ResponseEntity<AIChatResponse> generate(
             @RequestParam(value = "promptMessage", defaultValue = "결혼식 언제 할까")
             String promptMessage) {

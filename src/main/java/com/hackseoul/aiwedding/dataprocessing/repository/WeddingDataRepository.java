b4f86eed86ff207754fea8dd3584f1e6c6552df4
isun-dev <florachoi1124@naver.com>
2024-08-24 21:35:43 +0900

    feat: 네이버에서 시군구 가져오는 로직 추가1

---
+++ src/main/java/com/hackseoul/aiwedding/dataprocessing/repository/WeddingDataRepository.java
@@ -0,0 +1,15 @@
+package com.hackseoul.aiwedding.dataprocessing.repository;
+
+
+import com.hackseoul.aiwedding.crawling.entity.IweddingEnterprise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WeddingDataRepository extends JpaRepository<IweddingEnterprise, Long> {
+    @Query("SELECT data FROM IweddingEnterprise data")
+    List<IweddingEnterprise> findAllEnterprises();
+}
--- src/main/java/com/hackseoul/aiwedding/wedding/mypage/controller/MypageController.java
+++ src/main/java/com/hackseoul/aiwedding/wedding/mypage/controller/MypageController.java
@@ -1,15 +1,23 @@
 package com.hackseoul.aiwedding.wedding.mypage.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "마이페이지", description = "마이페이지에서 사용하는 API 입니다.")
 public class MypageController {
 
     // private final MypageServiceInterface mypageServiceInterface;
+
+    @PostMapping("api/v1/info")
+
+
+
 
     public void createMypage() {
 
@@ -26,4 +34,6 @@ public class MypageController {
     public void deleteMypage() {
 
     }
+
+
 }
